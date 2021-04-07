6856feb37aad41269447ea243731af784c1c06ed
Abilash Venkatesan <abilashvenkat6190@gmail.com>
2021-04-06 21:26:17 -0400

    Deck color can be changed now by the user

--- exercise/scripts/main.js
+++ exercise/scripts/main.js
@@ -54,8 +54,8 @@ window.addEventListener('DOMContentLoaded', function() {
   // - #green
   // - #orange
   // - #red
-  var deckElement = document.querySelector('.Cards');
-  var deckColor = window.location.hashh;
+  var deckElement = document.querySelector('.cards');
+  var deckColor = window.location.hash.substring(1);
 
   if (deckElement && deckColor) {
     var className = 'cards--' + deckColor;
